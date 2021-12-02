d70080e0e1325a8ea037be4b2eb041b6f99542d0
Ankur Prajapati <ankurp@geekyants.com>
2021-12-02 12:19:22 +0530

    connected to airtable db

---
+++ pages/api/getTodos.js
@@ -0,0 +1,10 @@
+import { table, minifyRecords, minifySingleRecord } from "./utils/Airtable";
+
+export default async function handler(req, res) {
+  try {
+    const records = await table.select({}).firstPage();
+    res.status(200).json(minifyRecords(records));
+  } catch (error) {
+    res.status(400).json({ message: "Oops! Something went wrong" });
+  }
+}
